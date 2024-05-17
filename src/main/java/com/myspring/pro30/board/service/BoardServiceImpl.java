38406d659739de7f33e503fb1c8afc4168a8a7fa
developerkjm <jmkang1984@gamil.com>
2024-05-17 10:46:37 +0900

    spring project

---
+++ src/main/java/com/myspring/pro30/board/service/BoardServiceImpl.java
@@ -0,0 +1,76 @@
+package com.myspring.pro30.board.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myspring.pro30.board.dao.BoardDAO;
+import com.myspring.pro30.board.vo.ArticleVO;
+import com.myspring.pro30.board.vo.ImageVO;
+
+
+@Service("boardService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class BoardServiceImpl  implements BoardService{
+	@Autowired
+	BoardDAO boardDAO;
+	
+	public List<ArticleVO> listArticles() throws Exception{
+		List<ArticleVO> articlesList =  boardDAO.selectAllArticlesList();
+        return articlesList;
+	}
+
+	/*
+	//단일이미지 업로드
+	@Override
+	public int addNewArticle(Map articleMap) throws Exception{
+		return boardDAO.insertNewArticle(articleMap);
+	}
+	*/
+	//다중 이미지 업로드
+	@Override
+	public int addNewArticle(Map articleMap) throws Exception{
+		int articleNO = boardDAO.insertNewArticle(articleMap);
+		articleMap.put("articleNO", articleNO);
+		boardDAO.insertNewImage(articleMap);
+		return articleNO;
+	}
+	//다중 이미지 화면
+	@Override
+	public Map viewArticle(int articleNO) throws Exception {
+		Map articleMap = new HashMap();
+		ArticleVO articleVO = boardDAO.selectArticle(articleNO);
+		List<ImageVO> imageFileList = boardDAO.selectImageFileList(articleNO);
+		articleMap.put("article", articleVO);
+		articleMap.put("imageFileList", imageFileList);
+		return articleMap;
+	}
+	
+	
+	 //단일 이미지 화면
+	/*
+	@Override
+	public ArticleVO viewArticle(int articleNO) throws Exception {
+		ArticleVO articleVO = boardDAO.selectArticle(articleNO);
+		return articleVO;
+	}
+	*/
+	
+	@Override
+	public void modArticle(Map articleMap) throws Exception {
+		boardDAO.updateArticle(articleMap);
+	}
+	
+	@Override
+	public void removeArticle(int articleNO) throws Exception {
+		boardDAO.deleteArticle(articleNO);
+	}
+	
+
+	
+}
